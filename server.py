5d8678c14a82b7e67b3ef22a60321823abdc9cfb
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-02-16 21:54:25 -0700

    Update server.py

--- server.py
+++ server.py
@@ -29,3 +29,41 @@ def pageNotFound(missing):
 
 if __name__=="__main__":
     app.run(debug=True)
+  
+
+# Second time solution
+
+# from flask import Flask, render_template
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     context = {
+#         "num" : 3,
+#         "color1" : "black",
+#         "color2" : "red"
+#     }
+#     return render_template("index.html", **context)
+
+# @app.route('/<int:num1>/<int:num2>')
+# def num_of_boxes(num1, num2):
+#     context = {
+#         "num1" : int(num1/2),
+#         "num2" : int(num2/2),
+#         "color1" : "black",
+#         "color2" : "red"
+#     }
+#     return render_template("index.html", **context)
+
+# @app.route('/<int:num1>/<int:num2>/<string:color1>/<string:color2>')
+# def num_of_boxes_define_color(num1, num2, color1, color2):
+#     context = {
+#         "num1" : int(num1/2),
+#         "num2" : int(num2/2),
+#         "color1" : color1,
+#         "color2" : color2
+#     }
+#     return render_template("index.html", **context)
+
+# if __name__=="__main__":
+#     app.run(debug=True)
